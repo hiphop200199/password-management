a056b83f69e6eb7131d0432253d8137043b77a1c
Eric Tsai <hiphop200199@gmail.com>
2023-04-24 11:49:26 +0800

    setting auth

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 //用下面這種方式找底下的元素更方便
 const signUpForm = document.getElementById("sign-up-form");
+let logout = document.querySelector(".logout");
+let management = document.querySelector(".management");
 signUpForm.addEventListener("submit",e=>{
     e.preventDefault();
     const email = signUpForm["sign-account"].value;
@@ -10,5 +12,14 @@ signUpForm.addEventListener("submit",e=>{
         let dialog = document.querySelector("dialog");
         dialog.close();
         signUpForm.reset();
+        logout.style.display='inline';
+        management.style.display="flex";
+    })
+})
+logout.addEventListener("click",e =>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        logout.style.display='none';
+        management.style.display='none';
     })
 })
